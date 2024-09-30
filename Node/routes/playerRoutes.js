831a59ef587c70b08a970275878ae6300e632ed7
kms040224 <kms0402246103@gmail.com>
2024-09-30 14:37:49 +0900

    [Feat]Routes 경로 설정

---
+++ Node/routes/playerRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');       //fs 모듈 추가
+const router = express.Router();
+
+//초기 자원 설정
+const initialResources =
+{
+    metal : 500,
+    crystal : 300,
+    deuterium : 100,
+
+}
+
+//글로벌 플레이어 객체 초기화
+global.players = {};                //글로벌 객체 초기화
+
+module.exports = router;                    //라우터 내보내기
